8941235f61545ae9e274689626de227ea43b9bb5
ItchiSushi <dpengmarais99@gmail.com>
2024-09-27 17:03:18 +0200

    Created Password Manager program and started coding.

---
+++ Password Manager/password_manager.py
@@ -0,0 +1,5 @@
+# Prompt the user to store a password or to add a new one.
+
+pwd = input("What is the master password")
+
+mode = input("Would you like to add a new password or view existing ones(view, add)? ")
