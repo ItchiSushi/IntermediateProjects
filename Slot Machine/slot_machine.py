cb5a7367e43f10e3974e05c1de7d87a54285ead0
ItchiSushi <dpengmarais99@gmail.com>
2024-10-08 21:01:10 +0200

    - Continued with the program. Added more functionality.

--- Slot Machine/slot_machine.py
+++ Slot Machine/slot_machine.py
@@ -14,9 +14,48 @@ symbol_count = {
     "D": 8
 }
 
-'''def get_slot_machine_spin(rows, cols, symbols):
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+
+    return winnings
+
+def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in symbol_count: '''
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+def print_slot_columns(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end = " | ")
+            else:
+                print(column[row], end = " | ")
+
+        print()
+
 
 def get_deposit():
     while True:
@@ -72,5 +111,8 @@ def main():
             break
 
     print(f"You are betting R {bet} on {lines} lines.  The total bet is R{total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_columns(slots)
 
 main()
