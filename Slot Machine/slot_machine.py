0088502ddf6c50680c785a5df5726ff7e7caa0a5
ItchiSushi <dpengmarais99@gmail.com>
2024-10-09 14:58:11 +0200

    - Completed the program. - Added Final functionality.

--- Slot Machine/slot_machine.py
+++ Slot Machine/slot_machine.py
@@ -13,9 +13,16 @@ symbol_count = {
     "C": 6,
     "D": 8
 }
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
 
 def check_winnings(columns, lines, bet, values):
     winnings = 0
+    winning_lines = []
     for line in range(lines):
         symbol = columns[0][line]
         for column in columns:
@@ -24,8 +31,9 @@ def check_winnings(columns, lines, bet, values):
                 break
         else:
             winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
 
-    return winnings
+    return winnings, winning_lines
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -98,8 +106,7 @@ def get_bet():
             print("That is not a valid amount!")
     return bet_amount
 
-def main():
-    balance = get_deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -115,4 +122,20 @@ def main():
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_columns(slots)
 
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won {winnings}!")
+    print("You won on lines: ", *winning_lines)
+
+    return  winnings - total_bet
+
+def main():
+    balance = get_deposit()
+    while True:
+        print(f"Current balance is {balance}")
+        answer = input("Press enter to play or q to quit:")
+        if answer.lower() == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with R{balance}")
 main()
